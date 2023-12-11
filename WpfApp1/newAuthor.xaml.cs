ad9598218f0e9d5ad565f1d80792ec5150a5ba3e
EGAProg <gameswithiy@gmail.com>
2023-12-11 09:07:51 +0300

    DataBase access C# example

---
+++ WpfApp1/newAuthor.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для newAuthor.xaml
+    /// </summary>
+    public partial class newAuthor : Window
+    {
+        public newAuthor()
+        {
+            InitializeComponent();
+        }
+
+        private void AddAuthorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NpgsqlConnection connection = ConnectionToDB.GetDbConnection();
+            try
+            {
+                string query = $"CALL INSERT_AUTORS('{fioTextBox.Text}', '{bdayTextBox.Text}');";
+                connection.Open();
+                NpgsqlCommand cmd = new(query, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                StatusLBL.Content = "Success!";
+            } 
+            catch (Exception ex)
+            {
+                StatusLBL.Content = ex;
+            }
+        }
+    }
+}
